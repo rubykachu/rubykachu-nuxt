7d8ddcbdeb5282e6db1e88373a6424975356cacf
at-minhtang <minh.tang@asiantech.vn>
2019-12-06 21:29:49 +0700

    refactor component category

---
+++ components/article/DialogCreateCategory.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-dialog v-model="toggleDialog" persistent max-width="600px">
+    <v-card>
+      <v-card-title>
+        <p class="headline">Thêm danh mục</p>
+      </v-card-title>
+      <v-divider></v-divider>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                v-model="category"
+                label="Tên danh mục"
+                prepend-icon="mdi-folder"
+                :error-messages="msgCategoryInvalid"
+                :success-messages="msgCreateSuccessCategory"
+                @focus="resetCategory"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click="closeDialog">Đóng</v-btn>
+        <v-btn color="blue darken-1" text @click="createCategory"
+          >Tạo danh mục</v-btn
+        >
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import { required } from 'vuelidate/lib/validators'
+
+export default {
+  props: {
+    dialog: {
+      type: Boolean,
+      default: false,
+      required: true
+    }
+  },
+  data() {
+    return {
+      toggleDialog: this.dialog,
+      msgCategoryInvalid: '',
+      msgCreateSuccessCategory: '',
+      category: null
+    }
+  },
+  watch: {
+    dialog(value) {
+      this.toggleDialog = value
+    }
+  },
+  validations: {
+    category: { required }
+  },
+  methods: {
+    closeDialog() {
+      this.toggleDialog = false
+      this.resetCategory()
+      this.$emit('click:close')
+    },
+    resetCategory() {
+      this.msgCreateSuccessCategory = ''
+      this.msgCategoryInvalid = ''
+      this.category = ''
+    },
+    async createCategory() {
+      this.$v.category.$touch()
+      if (!this.$v.category.$invalid) {
+        try {
+          await this.$store.dispatch('category/createCategory', {
+            name: this.category
+          })
+          this.msgCreateSuccessCategory = '* Tạo danh mục thành công'
+          this.category = ''
+        } catch (e) {
+          alert('Error: Please check console log')
+        }
+      } else {
+        this.msgCategoryInvalid = 'Vui lòng nhập danh mục'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/article/new.vue
+++ pages/article/new.vue
@@ -26,7 +26,13 @@
               @blur="$v.article.category_id.$touch()"
             >
               <template v-slot:append-outer>
-                <v-btn color="secondary" @click="openDialog" x-small dense text>
+                <v-btn
+                  color="secondary"
+                  @click="dialog = true"
+                  x-small
+                  dense
+                  text
+                >
                   <v-icon color="secondary">mdi-plus-circle</v-icon>
                 </v-btn>
               </template>
@@ -107,39 +113,7 @@
       </v-col>
     </v-row>
 
-    <!-- Dialog -->
-    <v-dialog v-model="dialog" persistent max-width="600px">
-      <v-card>
-        <v-card-title>
-          <p class="headline">Thêm danh mục</p>
-        </v-card-title>
-        <v-divider></v-divider>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12">
-                <v-text-field
-                  v-model="category"
-                  label="Tên danh mục"
-                  prepend-icon="mdi-folder"
-                  :error-messages="msgCategoryInvalid"
-                  :success-messages="msgCreateSuccessCategory"
-                  @focus="resetCategory"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-          </v-container>
-          <!-- <small class="success--text">{{ msgCreateSuccessCategory }}</small> -->
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="closeDialog">Đóng</v-btn>
-          <v-btn color="blue darken-1" text @click="createCategory"
-            >Tạo danh mục</v-btn
-          >
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <dialog-create-category :dialog="dialog" @click:close="dialog = false" />
   </v-container>
 </template>
 
@@ -147,24 +121,21 @@
 import moment from 'moment'
 import { required, minLength, numeric } from 'vuelidate/lib/validators'
 import Editor from '@/components/Editor.vue'
-import { resolve, reject } from 'q'
+import DialogCreateCategory from '@/components/article/DialogCreateCategory.vue'
 
 export default {
   components: {
-    Editor
+    Editor,
+    DialogCreateCategory
   },
   data() {
     return {
       dialog: false,
-      msgCategoryInvalid: '',
-      msgCreateSuccessCategory: '',
       toggleDatePicker: false,
-      category: null,
       article: this.newArticleObject()
     }
   },
   validations: {
-    category: { required },
     article: {
       title: {
         required,
@@ -246,35 +217,6 @@ export default {
           alert('Error: Please check console log')
           console.log(e)
         }
-      }
-    },
-    // Category Dialog
-    openDialog() {
-      this.dialog = true
-    },
-    closeDialog() {
-      this.dialog = false
-      this.resetCategory()
-    },
-    resetCategory() {
-      this.msgCreateSuccessCategory = ''
-      this.msgCategoryInvalid = ''
-      this.category = ''
-    },
-    async createCategory() {
-      this.$v.category.$touch()
-      if (!this.$v.category.$invalid) {
-        try {
-          await this.$store.dispatch('category/createCategory', {
-            name: this.category
-          })
-          this.msgCreateSuccessCategory = '* Tạo danh mục thành công'
-          this.category = ''
-        } catch (e) {
-          alert('Error: Please check console log')
-        }
-      } else {
-        this.msgCategoryInvalid = 'Vui lòng nhập danh mục'
       }
     }
   }
