95ee722084bf4513444e2ac312ce098c2dd1ceeb
at-minhtang <minh.tang@asiantech.vn>
2019-12-09 09:41:30 +0700

    style

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,23 @@
 <template>
   <v-container class="home-page">
     <v-row>
-      <v-col
-        class="mb-8"
-        cols="md-4"
-        sm="6"
-        v-for="article in articles"
-        :key="article.id"
-      >
+      <v-col class="mb-8" cols="md-4" sm="6" v-for="article in articles" :key="article.id">
         <article-item :article="article" />
       </v-col>
     </v-row>
 
     <v-row justify="center" align="center" class="pagination">
-      <span class="white--text mr-3">Page 2 of 2</span>
-      <v-btn color="primary" rounded class="px-5">Next</v-btn>
+      <v-btn color="primary" rounded class="px-5">
+        <v-icon class="pr-1">mdi-arrow-left-circle</v-icon>
+        Quay lại
+      </v-btn>
+
+      <span class="white--text mx-3">Trang 2 / 2</span>
+
+      <v-btn color="primary" rounded class="px-5">
+        Kế tiếp
+        <v-icon class="pl-1">mdi-arrow-right-circle</v-icon>
+      </v-btn>
     </v-row>
   </v-container>
 </template>
