00a4ab734e79cf3512ad2ed1dbd8b7f7e8c86f0d
at-minhtang <minh.tang@asiantech.vn>
2019-12-11 11:52:32 +0700

    remove code unnes

--- apis/ApiArticle.js
+++ apis/ApiArticle.js
@@ -2,18 +2,6 @@ import ApiClient from './ApiClient.js'
 import fs from '~/plugins/firebase'
 
 export default {
-  get(page = 1, perpage) {
-    return ApiClient.get(`/articles?&_page=${page}&_limit=${perpage}`)
-  },
-  find(id) {
-    return ApiClient.get('/articles/' + id)
-  },
-  create(article) {
-    return ApiClient.post('/articles', article)
-  },
-  where(query) {
-    return ApiClient.get(`/articles?${query}`)
-  },
   async fsCreate(article) {
     const result = await fs.collection('articles').add(article)
     article.id = result.id
--- apis/ApiCategory.js
+++ apis/ApiCategory.js
@@ -1,16 +1,6 @@
-import ApiClient from './ApiClient.js'
 import fs from '~/plugins/firebase'
 
 export default {
-  get() {
-    return ApiClient.get('/categories')
-  },
-  find(id) {
-    return ApiClient.get('/categories/' + id)
-  },
-  create(category) {
-    return ApiClient.post('/categories', category)
-  },
   async fsGet() {
     const snapshot = await fs.collection('categories').get()
     return snapshot.docs.map(doc => doc.data())
