5ecf90e7bc840768a9489d451f58e81a2d0249df
at-minhtang <minh.tang@asiantech.vn>
2019-12-20 09:23:11 +0700

    split component Author

---
+++ components/article/Author.vue
@@ -0,0 +1,31 @@
+<template>
+   <v-row class="section section--transparent author-info rounded-15" justify="center">
+      <div class="section__content--w75">
+        <div class="section__avatar">
+          <v-avatar size="80">
+            <img
+              src="/avatar.jpg"
+              alt="Minh Tang Q."
+            ></img>
+          </v-avatar>
+        </div>
+        <p class="section__title hover-link"><a class="white--text">Minh Tang Q.</a></p>
+        <p class="section__content">{{ introduceAuthor }} </p>
+      </div>
+    </v-row>
+</template>
+
+<script>
+import { introduceMysefl } from '@/mixins/helper'
+export default {
+    computed: {
+      introduceAuthor() {
+      return introduceMysefl()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/article/_id/index.vue
+++ pages/article/_id/index.vue
@@ -2,43 +2,31 @@
   <v-container fluid class="article-page px-0">
     <v-row>
       <v-col cols="12">
-      <v-card
-        class="d-flex align-start flex-column rounded-15"
-        color="grey"
-        min-width="100%"
-        min-height="400"
-        :img="featureImage"
-      >
-        <v-card class="pa-2 mb-auto" color="transparent" outlined>
-          <v-card-text>
-            <v-icon color="white" class="icon-size mr-1"
-              >mdi-calendar-week</v-icon
-            >
-            <span class="subheading white--text">{{ formatCreatedAt }}</span>
-            <v-icon color="white" class="icon-size ml-5 mr-1"
-              >mdi-circle-slice-5</v-icon
-            >
-            <span class="subheading white--text">{{ article.reading_time }} phút</span>
+        <v-card class="d-flex align-start flex-column rounded-15" color="grey" min-width="100%" min-height="400" :img="featureImage">
+          <v-card class="pa-2 mb-auto" color="transparent" outlined>
+            <v-card-text>
+              <v-icon color="white" class="icon-size mr-1">mdi-calendar-week</v-icon>
+              <span class="subheading white--text">{{ formatCreatedAt }}</span>
+              <v-icon color="white" class="icon-size ml-5 mr-1">mdi-circle-slice-5</v-icon>
+              <span class="subheading white--text">{{ article.reading_time }} phút</span>
 
-            <v-btn color="warning" :to="`/article/${article.slug}/update`" nuxt text small class="ml-2" v-if="isLogged">
-              <v-icon class="mr-1">mdi-lead-pencil</v-icon> Chỉnh sửa
-            </v-btn>
-          </v-card-text>
-        </v-card>
+              <v-btn color="warning" :to="`/article/${article.slug}/update`" nuxt text small class="ml-2" v-if="isLogged">
+                <v-icon class="mr-1">mdi-lead-pencil</v-icon> Chỉnh sửa
+              </v-btn>
+            </v-card-text>
+          </v-card>
 
-        <v-card class="pa-2" color="transparent" outlined>
-          <v-card-text>
-            <v-chip :color="article.category.color" class="px-8 white--text font-weight-bold">{{ article.category.name }}</v-chip>
-            <h1 class="pt-5 white--text article-page__title break-work">
-              {{ article.title }}
-            </h1>
-          </v-card-text>
+          <v-card class="pa-2" color="transparent" outlined>
+            <v-card-text>
+              <v-chip :color="article.category.color" class="px-8 white--text font-weight-bold">{{ article.category.name }}</v-chip>
+              <h1 class="pt-5 white--text article-page__title break-work">
+                {{ article.title }}
+              </h1>
+            </v-card-text>
+          </v-card>
         </v-card>
-      </v-card>
       </v-col>
-
     </v-row>
-
 
     <div class="article-page__content-wrapper mt-12 mb-20 pa-4">
       <v-row>
@@ -66,26 +54,14 @@
       </v-row>
     </div>
 
-    <v-row class="section section--transparent author-info rounded-15" justify="center">
-      <div class="section__content--w75">
-        <div class="section__avatar">
-          <v-avatar size="80">
-            <img
-              src="/avatar.jpg"
-              alt="Minh Tang Q."
-            ></img>
-          </v-avatar>
-        </div>
-        <p class="section__title hover-link"><a class="white--text">Minh Tang Q.</a></p>
-        <p class="section__content">{{ introduceAuthor }} </p>
-      </div>
-    </v-row>
+    <author />
   </v-container>
 </template>
 
 <script>
 import Prism from 'prismjs'
 import { mapGetters } from 'vuex'
+import Author from '@/components/article/Author.vue'
 import 'prismjs/themes/prism-okaidia.css'
 import 'prismjs/components/prism-ruby.min.js'
 import 'prismjs/components/prism-python.min.js'
@@ -93,25 +69,26 @@ import 'prismjs/components/prism-sql.min.js'
 import 'prismjs/components/prism-yaml.min.js'
 import 'prismjs/components/prism-bash.min.js'
 
-import { removeLineBreak, formatDate, introduceMysefl } from '@/mixins/helper'
+import { removeLineBreak, formatDate } from '@/mixins/helper'
 
 export default {
+  components: {
+    Author
+  },
   head() {
     return {
       title: this.article.title,
-      meta: [
-        { hid: 'description', name: 'description', content: removeLineBreak(this.article.description) }
-      ]
+      meta: [{ hid: 'description', name: 'description', content: removeLineBreak(this.article.description) }]
     }
   },
   data: () => ({
-    articleLink: '',
+    articleLink: ''
   }),
   async asyncData({ store, params }) {
     try {
-       const article = await store.dispatch('article/findArticle', params.id)
-       return { article }
-    } catch(e) {
+      const article = await store.dispatch('article/findArticle', params.id)
+      return { article }
+    } catch (e) {
       console.log(e)
       store.dispatch('toast/show')
     }
@@ -122,9 +99,6 @@ export default {
   },
   computed: {
     ...mapGetters('auth', ['isLogged']),
-    introduceAuthor() {
-      return introduceMysefl()
-    },
     formatCreatedAt() {
       return formatDate(this.article.created_at)
     },
@@ -134,10 +108,10 @@ export default {
   },
   methods: {
     copyToClipboard() {
-      const el = document.getElementById('copy-link-article');
-      el.value = this.articleLink;
-      el.select();
-      document.execCommand('copy');
+      const el = document.getElementById('copy-link-article')
+      el.value = this.articleLink
+      el.select()
+      document.execCommand('copy')
 
       this.$store.dispatch('toast/show', 'Đã coppy liên kết bài viết vào Clipboard')
     }
