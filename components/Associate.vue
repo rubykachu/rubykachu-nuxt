de4a66c8904f1e85da68a8b04e9947309c6a2736
at-minhtang <minh.tang@asiantech.vn>
2019-12-20 14:24:03 +0700

    f

--- components/Associate.vue
+++ components/Associate.vue
@@ -3,7 +3,7 @@
     <a href="https://www.facebook.com/tangquocminh" target="blank" rel="nofollow"
       ><v-icon color="white" class="px-1">mdi-facebook-box</v-icon></a
     >
-    <a href="https://github.com/rubykachu" target="blank" rel="nofollow"><v-icon color="white" class="px-1">mdi-github-circle</v-icon></a>
+    <a href="https://github.com/" target="blank" rel="nofollow"><v-icon color="white" class="px-1">mdi-github-circle</v-icon></a>
     <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=vhquocminhit@gmail.com" target="blank" rel="nofollow"
       ><v-icon color="white" class="px-1">mdi-gmail</v-icon></a
     >
